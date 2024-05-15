222b29558fb8b8fc5f8cf5e3a8f9e1c3d6b25aac
ankitaDeveloper01 <pawarankita0405@gmail.com>
2024-05-15 22:46:23 +0530

    add name

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const app =  express()
 app.listen(5001, () => console.log("API is running on port"))
 
 app.get('/pop', (req,res) => res.json('My API running'))
+
+app.push('/name', (req,res) => res.json('chitti robot 1.0'))
