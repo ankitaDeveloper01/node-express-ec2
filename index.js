e3138772299c22340be3d5f6cfeaa16acf087f84
ankitaDeveloper01 <pawarankita0405@gmail.com>
2024-05-15 17:27:49 +0530

    pop msg

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import express from 'express'
 const app =  express()
 app.listen(5001, () => console.log("API is running on port"))
 
-app.get('/', (req,res) => res.json('My API running'))
+app.get('/pop', (req,res) => res.json('My API running'))
